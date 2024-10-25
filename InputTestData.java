2cc969e8f578379d86d4bcbe7081797b9ba5a0c2
ChandraSekhar-Testing <chandra.se.biswal@gmail.com>
2024-10-25 18:10:06 +0530

    Second commit

---
+++ InputTestData.java
@@ -0,0 +1,21 @@
+package caw.util;
+import java.util.List;
+import java.util.Map;
+
+public class InputTestData {
+
+		    public static final String URL = "https://testpages.herokuapp.com/styled/tag/dynamic-table.html";
+
+		    public static final List<Map<String, String>> TABLE_DATA = List.of(
+		            Map.of("name", "Bob", "age", "20", "gender", "male"),
+		            Map.of("name", "George", "age", "42", "gender", "male"),
+		            Map.of("name", "Sara", "age", "42", "gender", "female"),
+		            Map.of("name", "Conor", "age", "40", "gender", "male"),
+		            Map.of("name", "Jennifer", "age", "42", "gender", "female")
+		    );
+		
+
+
+	}
+
+
